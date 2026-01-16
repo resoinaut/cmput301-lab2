7319aae368e6898fc8cdfba863ddfdc527cfb069
resoinaut <resoinaut@gmail.com>
2026-01-15 23:11:34 -0700

    finish

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    Button button;
+
+    Button button_city_add;
+    Button button_city_delete;
+    Button button_confirm;
+
+    LinearLayout bottom_bar;
+
+    EditText city_input;
+
+    int pos = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        button_city_add    = findViewById(R.id.button_city_add);
+        button_city_delete = findViewById(R.id.button_city_delete);
+        button_confirm     = findViewById(R.id.button_confirm);
+
+        city_input = findViewById(R.id.city_input);
+
+        bottom_bar = findViewById(R.id.bottom_bar);
+        bottom_bar.setVisibility(View.GONE);
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                pos = position;
+            }
+        });
+
+        button_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "CONFIRMED", Toast.LENGTH_SHORT).show();
+                dataList.add(city_input.getText().toString());
+                city_input.getText().clear();
+                cityAdapter.notifyDataSetChanged();
+                bottom_bar.setVisibility(View.GONE);
+            }
+        });
+
+        button_city_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottom_bar.setVisibility(View.VISIBLE);
+                pos = -1;
+            }
+        });
+
+        button_city_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottom_bar.setVisibility(View.GONE);
+                if (pos != -1)
+                {
+                    dataList.remove(pos);
+                    cityAdapter.notifyDataSetChanged();
+                    pos = -1;
+                }
+            }
+        });
+    }
+}
